fff064b60fa4f3c7b9f0e7e8c480674ad8a06373
Nikhil Dubbaka <info@technikh.com>
2016-06-10 15:08:35 -0400

    Working Api with multiple content types.

--- PathNavigationGenerator.php
+++ PathNavigationGenerator.php
@@ -50,100 +50,59 @@ class PathNavigationGenerator implements GeneratorInterface
         $this->PathNavigationProvider = $PathNavigationProvider;
         $this->permalinkFactory = $permalinkFactory;
     }
+    
+    private function processContentTypeDataGeneration($providerData, &$source, $datedPostData)
+    {
+      $generatedSources = array();
+      foreach ($providerData as $post) {
+        // Get post URL Path
+        $sourcePermalinkFactory = new SourcePermalinkFactory('');
+        $permalink = $sourcePermalinkFactory->create($post);
+        $post_relative_url = $permalink->relativeUrlPath();
+        $post_relative_url = trim($post_relative_url, '/');
+        //ladybug_dump($post_relative_url);
+        $post_relative_url_array = explode('/', $post_relative_url);
+        $length = count($post_relative_url_array);
+        $post_relative_url_array = array_splice($post_relative_url_array, 1, $length-2);
+        for($i = 1; $i <= count($post_relative_url_array); $i++) {
+          $clone_post_relative_url_array = $post_relative_url_array;
+          $spliced_arr = array_splice($clone_post_relative_url_array, 0, $i);
+          $spliced_relative_url = implode('/', $spliced_arr);
+          
+          $pathGeneratedSource = $source->duplicate(
+              $source->sourceId().':path='."api/v1/categories/$spliced_relative_url/"
+              );
+          $pathGeneratedSource->data()->set('permalink', "api/v1/categories/$spliced_relative_url/");
+          $pathGeneratedSource->data()->set('path', "api/v1/categories/$spliced_relative_url/");
+          
+          $post_data_url_array = explode('/', $spliced_relative_url);
+          $clone_datedPostData = $datedPostData;
+          foreach($post_data_url_array as $directory_name){
+            $clone_datedPostData = $clone_datedPostData[$directory_name];
+          }
+          $pathGeneratedSource->data()->set('path_posts', $clone_datedPostData['posts']);
+          //$generatedPaths[] = "api/v1/categories/$spliced_relative_url/";
+          $generatedSources[] = $pathGeneratedSource;
+        }
+      }
+      return $generatedSources;
+    }
 
     /**
      * {@inheritDoc}
      */
     public function generate(SourceInterface $source)
     {
-        $generatedPaths = array();
-        //$generatedMonths = array();
         $generatedSources = array();
-
         $datedPostData = $this->PathNavigationProvider->provideData();
-        //print_r($datedPostData['tn_directories']);
-        //print "444".print_r($datedPostData['tn_directories'], TRUE)."555". "\r\n";
-        foreach ($this->dataProvider->provideData() as $post) {
-          // Get post URL Path
-          $sourcePermalinkFactory = new SourcePermalinkFactory('');
-          $permalink = $sourcePermalinkFactory->create($post);
-          $post_relative_url = $permalink->relativeUrlPath();
-          //print " 111 ".$post_relative_url." 222 ". "\r\n";
-          $post_relative_url = trim($post_relative_url, '/');
-          $post_relative_url_array = explode('/', $post_relative_url);
-          $length = count($post_relative_url_array);
-          $post_relative_url_array = array_splice($post_relative_url_array, 1, $length-2);
-          for($i = 1; $i <= count($post_relative_url_array); $i++) {
-            $clone_post_relative_url_array = $post_relative_url_array;
-            $spliced_arr = array_splice($clone_post_relative_url_array, 0, $i);
-            $spliced_relative_url = implode('/', $spliced_arr);
-            //$arr[$spliced_relative_url]['posts'][] = "nikhil";
-            
-            $pathGeneratedSource = $source->duplicate(
-                $source->sourceId().':path='."api/v1/categories//$spliced_relative_url/"
-                );
-            $pathGeneratedSource->data()->set('permalink', "api/v1/categories/$spliced_relative_url/");
-            $pathGeneratedSource->data()->set('path', "api/v1/categories/$spliced_relative_url/");
-            
-            $post_data_url_array = explode('/', $spliced_relative_url);
-            $clone_datedPostData = $datedPostData;
-            foreach($post_data_url_array as $directory_name){
-              $clone_datedPostData = $clone_datedPostData[$directory_name];
-            }
-            $pathGeneratedSource->data()->set('path_posts', $clone_datedPostData['posts']);
-            
-            $generatedPaths[] = "api/v1/categories/$spliced_relative_url/";
-            $generatedSources[] = $pathGeneratedSource;
-          }
-          
-          
-          
-          
-          /*$pathGeneratedSource = $source->duplicate(
-              $source->sourceId().':path='.'blog/dir1/'
-              );
-          $pathGeneratedSource->data()->set('permalink', 'blog/dir1/');
-          $pathGeneratedSource->data()->set('path', 'blog/dir1/');
-          $pathGeneratedSource->data()->set('path_posts', $datedPostData['dir1']['posts']);
-          $generatedPaths[] = 'blog/dir1/';
-          $generatedSources[] = $pathGeneratedSource;*/
-          
-            /*$date = \DateTime::createFromFormat('U', 0);
-            if ($post->date() !== "") {
-                $date = \DateTime::createFromFormat('U', $post->date());
-            }
 
-            $year = $date->format('Y');
-            $month = $date->format('m');
-
-            if (in_array($year.'-'.$month, $generatedMonths)) {
-                continue;
-            }
-
-            $monthGeneratedSource = $source->duplicate(
-                $source->sourceId().':year='.$year.':month='.$month
-            );
-            $monthGeneratedSource->data()->set('permalink', $this->permalinkFactory->getMonth($year, $month));
-            $monthGeneratedSource->data()->set('year', $year);
-            $monthGeneratedSource->data()->set('month', $month);
-            $monthGeneratedSource->data()->set('path_posts', $datedPostData[$year]['months'][$month]['posts']);
-            $generatedMonths[] = $year.'-'.$month;
-            $generatedSources[] = $monthGeneratedSource;
-
-            if (in_array($year, $generatedYears)) {
-                continue;
-            }
-
-            $yearGeneratedSource = $source->duplicate(
-                $source->sourceId().':year='.$year
-            );
-            $yearGeneratedSource->data()->set('permalink', $this->permalinkFactory->getYear($year));
-            $yearGeneratedSource->data()->set('year', $year);
-            $yearGeneratedSource->data()->set('path_posts', $datedPostData[$year]['posts']);
-            $generatedYears[] = $year;
-            $generatedSources[] = $yearGeneratedSource;*/
-        }
-
+        $videosPathGeneratedSource = $this->processContentTypeDataGeneration($this->dataProvider->provideData(), $source, $datedPostData);
+        $articlesPathGeneratedSource = $this->processContentTypeDataGeneration($this->articlesDataProvider->provideData(), $source, $datedPostData);
+        
+        //$generatedSources = $generatedSources + $videosPathGeneratedSource;
+        //$generatedSources = $generatedSources + $articlesPathGeneratedSource;
+        //$generatedSources[] = $articlesPathGeneratedSource;
+        $generatedSources = array_merge_recursive($videosPathGeneratedSource, $articlesPathGeneratedSource);
         return $generatedSources;
     }
 }
--- PathNavigationProvider.php
+++ PathNavigationProvider.php
@@ -37,9 +37,35 @@ class PathNavigationProvider implements DataProviderInterface
         $this->articlesDataProvider = $articlesDataProviderManager;
     }
     
-    private function processContentTypeData($dataProvider)
+    private function processContentTypeData($providerData, &$data, &$all_paths, $content_type)
     {
-      
+      foreach ($providerData as $post) {
+        //https://github.com/sculpin/sculpin/blob/master/src/Sculpin/Bundle/PaginationBundle/PaginationGenerator.php#L101
+        $sourcePermalinkFactory = new SourcePermalinkFactory('');
+        $permalink = $sourcePermalinkFactory->create($post);
+        $post_relative_url = $permalink->relativeUrlPath();
+        $post_relative_url = trim($post_relative_url, '/');
+        $post_relative_url_array = explode('/', $post_relative_url);
+        $length = count($post_relative_url_array);
+        $post_relative_url_array = array_splice($post_relative_url_array, 1, $length-2);
+        for($i = 1; $i <= $length; $i++) {
+          $clone_post_relative_url_array = $post_relative_url_array;
+          $spliced_arr = array_splice($clone_post_relative_url_array, 0, $i);
+          $spliced_relative_url = implode('/', $spliced_arr);
+          $arr[$spliced_relative_url]['posts'][$post_relative_url] = $post;
+          $all_paths[$spliced_relative_url][] = $spliced_relative_url;
+        }
+        foreach ($arr as $key=>$val) {
+          $r = & $data;
+          foreach (explode("/", $key) as $key) {
+            if (!isset($r[$key])) {
+              $r[$key] = array();
+            }
+            $r = & $r[$key];
+          }
+          $r = $val;
+        }
+      }
     }
 
     /**
@@ -58,81 +84,22 @@ class PathNavigationProvider implements DataProviderInterface
             return $this->computedData;
         }
 
+        $videos_data = array();
+        $articles_data = array();
         $data = array();
         $out = array();
         
         $all_paths = array();
         $hierarchical_paths = array();
-        foreach ($this->dataProvider->provideData() as $post) {
-          //print "111 Provider ".$post->sourceId()."222". "\r\n";
-          //https://github.com/sculpin/sculpin/blob/master/src/Sculpin/Bundle/PaginationBundle/PaginationGenerator.php#L101
-          $sourcePermalinkFactory = new SourcePermalinkFactory('');
-          $permalink = $sourcePermalinkFactory->create($post);
-          $post_relative_url = $permalink->relativeUrlPath();
-          //print " 111 ".$post_relative_url." 222 ". "\r\n";
-          $post_relative_url = trim($post_relative_url, '/');
-          $post_relative_url_array = explode('/', $post_relative_url);
-          $length = count($post_relative_url_array);
-          $post_relative_url_array = array_splice($post_relative_url_array, 1, $length-2);
-          for($i = 1; $i <= count($post_relative_url_array); $i++) {
-            $clone_post_relative_url_array = $post_relative_url_array;
-            $spliced_arr = array_splice($clone_post_relative_url_array, 0, $i);
-            $spliced_relative_url = implode('/', $spliced_arr);
-            $arr[$spliced_relative_url]['posts'][] = $post;
-            $all_paths[$spliced_relative_url][] = $spliced_relative_url;
-          }
-          //ladybug_dump($arr);
-          foreach ($arr as $key=>$val) {
-            $r = & $data;
-            foreach (explode("/", $key) as $key) {
-              if (!isset($r[$key])) {
-                $r[$key] = array();
-              }
-              $r = & $r[$key];
-            }
-            $r = $val;
-            
-       
-            
-            //var_export($data);
-            //print "------------999 Provider ".print_r($val, TRUE)."888". "\r\n";
-          }
-          //ladybug_dump($data);
-          //break;
-          
-          //print "111 Provider ".print_r($out, TRUE)."222". "\r\n";
-          //$data = $out;
-          
-          /*$data['dir1']['posts'][] = "nikhil";
-          $data['dir1']['months']['dir11']['posts'][] = "nikhil";*/
-          //var_dump($data);
-          //break;
-          /*
-            $date = \DateTime::createFromFormat('U', 0);
-            if ($post->date() !== "") {
-                $date = \DateTime::createFromFormat('U', $post->date());
-            }
-
-            $year = $date->format('Y');
-            $month = $date->format('m');
-            $keyDate = \DateTime::createFromFormat('Y-m',$year.'-'.$month);
-
-            if (!isset($data[$year])) {
-                $data[$year] = array(
-                    'posts' => array(),
-                    'months' => array(),
-                    'date' => $keyDate
-                );
-            }
-
-            if (!isset($data[$year]['months'])) {
-                $data[$year]['months'] = array('posts' => array(), 'date' => $keyDate);
-            }
-
-            $data[$year]['posts'][] = $post;
-            $data[$year]['months'][$month]['posts'][] = $post;*/
-        }
-        ladybug_dump($all_paths);
+        
+        $this->processContentTypeData($this->dataProvider->provideData(), $videos_data, $all_paths, "videos");
+        //ladybug_dump($data);
+        $this->processContentTypeData($this->articlesDataProvider->provideData(), $articles_data, $all_paths, "articles");
+        $data = array_merge_recursive($videos_data, $articles_data);
+        
+        
+        
+        ladybug_dump($data);
         foreach ($all_paths as $key=>$val) {
           $r = & $hierarchical_paths;
           foreach (explode("/", $key) as $key) {
@@ -143,8 +110,7 @@ class PathNavigationProvider implements DataProviderInterface
           }
           //$r = $val;
         }
-        ladybug_dump($hierarchical_paths);
-        //print "111 Provider ".print_r($data, TRUE)."222". "\r\n";
+        //ladybug_dump($hierarchical_paths);
         $data['tn_directories'] = $hierarchical_paths;
         /*
         $data['tn_directories'][] = array("name" => "SSC",
@@ -164,7 +130,6 @@ class PathNavigationProvider implements DataProviderInterface
         $data['tn_directories'][] = array("name" => "9th");
         */
         $this->computedData = $data;
-        //var_dump(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
         return $data;
     }
 }
