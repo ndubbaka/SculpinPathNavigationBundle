7018ed81f81d6229c1dbe53b2b1c9ca35d653fe8
Nikhil Dubbaka <info@technikh.com>
2016-11-03 11:39:31 -0400

    Adding thumbnails to API and malink it work for wiki in schools.

--- PathNavigationProvider.php
+++ PathNavigationProvider.php
@@ -45,39 +45,55 @@ class PathNavigationProvider implements DataProviderInterface
         $permalink = $sourcePermalinkFactory->create($post);
         $post_relative_url = $permalink->relativeUrlPath();
         $post_relative_url = trim($post_relative_url, '/');
-        $post_relative_url_array = explode('/', $post_relative_url);
-        $length = count($post_relative_url_array);
-        // 2 to account preceeding path: open-curricula-files/_videos in sculpin_kernel.yml
-        $post_relative_url_array = array_splice($post_relative_url_array, 2, $length-3);
-        for($i = 2; $i <= $length; $i++) {
-          $clone_post_relative_url_array = $post_relative_url_array;
-          $spliced_arr = array_splice($clone_post_relative_url_array, 0, $i);
-          $spliced_relative_url = implode('/', $spliced_arr);
-          $arr[$spliced_relative_url]['posts'][$post_relative_url] = $post;
-          $arr[$spliced_relative_url]['apis']['items'][$post_relative_url] = array(
-              "title" => $post->title(),
-              "url" => $post_relative_url,
-              "content" => $post->content(),
-              "tags" => $post->data()->get('tags'),
-          );
-          if($content_type == "videos"){
-            $arr[$spliced_relative_url]['apis']['items'][$post_relative_url]["youtube_id"] = $post->data()->get('youtube_id');
-          }elseif($content_type == "articles"){
-            $arr[$spliced_relative_url]['apis']['items'][$post_relative_url]["article_ref"] = $post->data()->get('article_ref');
-          }
-          $all_paths[$spliced_relative_url][] = $spliced_relative_url;
-        }
-        foreach ($arr as $key=>$val) {
-          $r = & $data;
-          foreach (explode("/", $key) as $key) {
-            if (!isset($r[$key])) {
-              $r[$key] = array();
+        if($this->endsWith($post_relative_url, ".md")){
+          $post_relative_url_array = explode('/', $post_relative_url);
+          $length = count($post_relative_url_array);
+          // 2 to account preceeding path: open-curricula-files/_videos in sculpin_kernel.yml
+          $post_relative_url_array = array_splice($post_relative_url_array, 2, $length-3);
+          for($i = 1; $i <= $length; $i++) {
+            $clone_post_relative_url_array = $post_relative_url_array;
+            $spliced_arr = array_splice($clone_post_relative_url_array, 0, $i);
+            $spliced_relative_url = implode('/', $spliced_arr);
+            $arr[$spliced_relative_url]['posts'][$post_relative_url] = $post;
+            $arr[$spliced_relative_url]['apis']['items'][$post_relative_url] = array(
+                "title" => $post->title(),
+                "url" => $post_relative_url,
+                "content" => $post->content(),
+                "tags" => $post->data()->get('tags'),
+            );
+            $thumbnail_urls = $post->data()->get('thumbnail_urls');
+            if(!empty($thumbnail_urls)){
+              $arr[$spliced_relative_url]['apis']['items'][$post_relative_url]["thumbnails"] = $thumbnail_urls;
             }
-            $r = & $r[$key];
+            if($content_type == "videos"){
+              $arr[$spliced_relative_url]['apis']['items'][$post_relative_url]["youtube_id"] = $post->data()->get('youtube_id');
+            }elseif($content_type == "articles"){
+              $arr[$spliced_relative_url]['apis']['items'][$post_relative_url]["article_ref"] = $post->data()->get('article_ref');
+            }
+            $all_paths[$spliced_relative_url][] = $spliced_relative_url;
           }
-          $r = $val;
+          foreach ($arr as $key=>$val) {
+            $r = & $data;
+            foreach (explode("/", $key) as $key) {
+              if (!isset($r[$key])) {
+                $r[$key] = array();
+              }
+              $r = & $r[$key];
+            }
+            $r = $val;
+          }
         }
       }
+    }
+
+    public function endsWith($haystack, $needle)
+    {
+      $length = strlen($needle);
+      if ($length == 0) {
+        return true;
+      }
+
+      return (substr($haystack, -$length) === $needle);
     }
 
     /**
@@ -111,7 +127,7 @@ class PathNavigationProvider implements DataProviderInterface
         
         
         
-        //ladybug_dump($data);
+        //ladybug_dump($all_paths);
         foreach ($all_paths as $key=>$val) {
           $r = & $hierarchical_paths;
           foreach (explode("/", $key) as $key) {
@@ -123,25 +139,47 @@ class PathNavigationProvider implements DataProviderInterface
           //$r = $val;
         }
         //ladybug_dump($hierarchical_paths);
-        $data['tn_directories'] = $hierarchical_paths;
-        /*
-        $data['tn_directories'][] = array("name" => "SSC",
-            "items" => array(
-                array("name" => "Biology",
-                    "items" => array(
-                        array("name" => "Nutrition",
-                    
-                        ),
-                        array("name" => "Respiration",
-                        
-                        ),
-                    ),
-                ),
-            ),
-        );
-        $data['tn_directories'][] = array("name" => "9th");
-        */
+        $api_tn_directories = $this->hierarchical_path_array_to_api_array($hierarchical_paths);
+        //ladybug_dump($api_tn_directories);
+        $data['tn_directories'] = $api_tn_directories;
         $this->computedData = $data;
         return $data;
+    }
+
+    public function hierarchical_path_array_to_api_array($hierarchical_paths, $path = '')
+    {
+      $output = array();
+      foreach ($hierarchical_paths as $key=>$val) {
+        $output[$key]['name'] = $key;
+        $output[$key]['path'] = $path . "/$key";
+        $thumbnail_url = $this->get_thumbnail_from_path($path . "/$key");
+        if(!empty($thumbnail_url)){
+          $output[$key]['thumbnails'] = array($thumbnail_url);
+        }
+        if(is_array($val) && !empty($val)){
+          $output[$key]['items'] = $this->hierarchical_path_array_to_api_array($val, $path."/$key");
+        }
+      }
+      return $output;
+    }
+
+    public function get_thumbnail_from_path($path)
+    {
+      // $path: /Wikipedia -english/Mathematics/Discrete mathematics
+      // getcwd(): /Users/f3wztbn/gitlab_repos/TechNikh/open-curricula
+      // output_dev: open-curricula-files/_articles/Wikipedia -english/Mathematics/Discrete mathematics/thumb.ext
+      // thumb.png or thumb.jpg
+
+      if(file_exists("source/open-curricula-files/_articles{$path}/thumb.png")){
+        return "/open-curricula-files/_articles{$path}/thumb.png";
+      }elseif(file_exists("source/open-curricula-files/_articles{$path}/thumb.jpg")){
+        return "/open-curricula-files/_articles{$path}/thumb.jpg";
+      }elseif(file_exists("source/open-curricula-files/_videos{$path}/thumb.png")){
+        return "/open-curricula-files/_videos{$path}/thumb.png";
+      }elseif(file_exists("source/open-curricula-files/_videos{$path}/thumb.jpg")){
+        return "/open-curricula-files/_videos{$path}/thumb.jpg";
+      }
+
+      return '';
     }
 }
